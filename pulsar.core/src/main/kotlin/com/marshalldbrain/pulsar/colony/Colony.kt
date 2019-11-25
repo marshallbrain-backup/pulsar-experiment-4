238eeb4fafa4e95f6956a5e4bf48cef906b75d96
marshallbrain <marshalldbrain@gmail.com>
2019-11-25 09:27:20 -0500

    Initialize values

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/Colony.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/Colony.kt
@@ -1,4 +1,25 @@
 package com.marshalldbrain.pulsar.colony
 
-class Colony {
+import com.marshalldbrain.pulsar.colony.districts.District
+import com.marshalldbrain.pulsar.colony.districts.DistrictType
+
+data class Colony(val districtTypes: Map<String, DistrictType>) {
+	
+	val districts = MutableList(5) {
+		District(DistrictType(""))
+	}
+	
+	init {
+		
+		val startingDistrictTypes = districtTypes.values.filter { it.possible() }.sortedBy { it.id }
+		for (i in min(districts.indices, startingDistrictTypes.indices)) {
+			districts[i].type = startingDistrictTypes[i]
+		}
+		
+	}
+	
+	private fun min(a: IntRange, b: IntRange): IntRange {
+		return if (a.last > b.last) b else a
+	}
+	
 }
