8504cfe6c1373f5a884940f0375dffcd44920722
marshallbrain <marshalldbrain@gmail.com>
2019-11-25 09:27:39 -0500

    Add functions

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/districts/DistrictType.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/districts/DistrictType.kt
@@ -4,8 +4,18 @@ import com.marshalldbrain.pulsar.resources.Resource
 
 data class DistrictType (
 	val id: String,
+	val time: Int = 0,
 	val cost: List<Resource> = emptyList(),
 	val production: List<Resource> = emptyList(),
 	val upkeep: List<Resource> = emptyList()
 ) {
+	
+	fun possible(): Boolean {
+		return true
+	}
+	
+	fun starting(): Boolean {
+		return true
+	}
+	
 }
