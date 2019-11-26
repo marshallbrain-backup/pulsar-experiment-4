10366280de4831ff999dcf4eead3828897115aee
marshallbrain <marshalldbrain@gmail.com>
2019-11-26 13:28:06 -0500

    Make possible and starting values

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/districts/DistrictType.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/districts/DistrictType.kt
@@ -5,17 +5,10 @@ import com.marshalldbrain.pulsar.resources.Resource
 data class DistrictType (
 	val id: String,
 	val time: Int = 0,
+	val possible: Boolean = true,
+	val starting: Boolean = false,
 	val cost: List<Resource> = emptyList(),
 	val production: List<Resource> = emptyList(),
 	val upkeep: List<Resource> = emptyList()
 ) {
-	
-	fun possible(): Boolean {
-		return true
-	}
-	
-	fun starting(): Boolean {
-		return true
-	}
-	
 }
