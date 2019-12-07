13e19971d2b4ae995fbc21ee2f38811e8d5509b7
marshallbrain <marshalldbrain@gmail.com>
2019-12-07 10:13:49 -0500

    Rework gradle build script
    Replaced all code that pointed to the use of the jvm with generic code. Allows for the use of common modules and modules for different platforms.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,21 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-    kotlin("jvm") version "1.3.50"
-}
-
-dependencies {
-
-}
-
-allprojects {
-    
-    apply {
-        plugin("kotlin")
-    }
-    
-    version = 0.1
+buildscript {
     
     repositories {
         mavenCentral()
     }
     
     dependencies {
-        
-        implementation(kotlin("stdlib-jdk8"))
-        
+        classpath(kotlin("gradle-plugin", version = "1.3.61"))
     }
     
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+}
+
+allprojects {
+    
+    version = "0.1"
+    
+    repositories {
+        mavenCentral()
     }
     
 }
