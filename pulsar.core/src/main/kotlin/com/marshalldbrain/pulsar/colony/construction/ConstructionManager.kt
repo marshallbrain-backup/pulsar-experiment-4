eed580e1e6449e44fdfa16be5f85a03c93e579d4
marshallbrain <marshalldbrain@gmail.com>
2020-01-20 17:44:58 -0500

    Implement functionality

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/construction/ConstructionManager.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/construction/ConstructionManager.kt
@@ -1,5 +1,32 @@
 package com.marshalldbrain.pulsar.colony.construction
 
+import com.marshalldbrain.ion.collections.queueOf
+
 class ConstructionManager {
+	
+	private val constructionQueue = queueOf<ConstructionTask>()
+	
+	fun addToQueue(task: ConstructionTask) {
+		constructionQueue.add(task)
+		if (constructionQueue.size == 1) {
+			constructionQueue.peek()!!.onStart()
+		}
+	}
+	
+	fun getQueue(): Collection<ConstructionTask> {
+		return constructionQueue
+	}
+	
+	fun tick(amount: Int) {
+		var remaining = amount
+		while (remaining > 0 && constructionQueue.size > 0) {
+			remaining -= constructionQueue.peek()!!.passTime(amount)
+			constructionQueue.peek()!!.whileConstructing()
+			if (remaining > 0) {
+				constructionQueue.poll()!!.onFinnish()
+				constructionQueue.peek()!!.onStart()
+			}
+		}
+	}
 	
 }
