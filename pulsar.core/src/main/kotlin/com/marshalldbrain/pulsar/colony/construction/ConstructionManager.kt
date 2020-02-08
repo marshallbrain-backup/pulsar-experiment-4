147545c2316b83f51101bc390367ed73d582afac
marshallbrain <marshalldbrain@gmail.com>
2020-02-07 21:19:04 -0500

    Redo the tick function

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/construction/ConstructionManager.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/construction/ConstructionManager.kt
@@ -4,27 +4,18 @@ import com.marshalldbrain.ion.collections.queueOf
 
 class ConstructionManager {
 	
-	private val constructionQueue = queueOf<ConstructionTask>()
+	val constructionQueue = queueOf<ConstructionTask>()
 	
 	fun addToQueue(task: ConstructionTask) {
 		constructionQueue.add(task)
-		if (constructionQueue.size == 1) {
-			constructionQueue.peek()!!.onStart()
-		}
-	}
-	
-	fun getQueue(): Collection<ConstructionTask> {
-		return constructionQueue
 	}
 	
 	fun tick(amount: Int) {
 		var remaining = amount
-		while (remaining > 0 && constructionQueue.size > 0) {
-			remaining -= constructionQueue.peek()!!.passTime(amount)
-			constructionQueue.peek()!!.whileConstructing()
-			if (remaining > 0) {
-				constructionQueue.poll()!!.onFinnish()
-				constructionQueue.peek()!!.onStart()
+		while (remaining > 0 && !constructionQueue.isEmpty()) {
+			remaining = constructionQueue.peek()!!.passTime(remaining)
+			if (remaining >= 0) {
+				constructionQueue.poll()
 			}
 		}
 	}
