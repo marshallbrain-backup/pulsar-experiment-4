2fa74db8bfb63b451efac9d958974e3754de6b09
marshallbrain <marshalldbrain@gmail.com>
2020-02-11 10:43:40 -0500

    Add operator functions

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/Resource.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/Resource.kt
@@ -2,6 +2,19 @@ package com.marshalldbrain.pulsar.resources
 
 data class Resource (
 	val type: ResourceType,
-	val amount: Int = 0
+	var amount: Int = 0
 ) {
+	
+	operator fun plusAssign(resource: Resource) {
+		if (type == resource.type) {
+			amount += resource.amount
+		}
+	}
+	
+	operator fun minusAssign(resource: Resource) {
+		if (type == resource.type) {
+			amount -= resource.amount
+		}
+	}
+	
 }
