65de2936eec03f6d596afa8d725bb81d9e97990a
marshallbrain <marshalldbrain@gmail.com>
2020-02-11 10:42:20 -0500

    Implement total production calculation

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceBucket.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceBucket.kt
@@ -1,4 +1,21 @@
 package com.marshalldbrain.pulsar.resources
 
-class ResourceBucket {
+class ResourceBucket (vararg sources: List<ResourceCollection>) {
+	
+	private val sourceList = sources.asList()
+	
+	val totalProduction: Set<Resource>
+		get() {
+			
+			val production = mutableMapOf<ResourceType, Resource>()
+			
+			sourceList.forEach { s -> s.forEach {
+				production += it.production
+				production -= it.upkeep
+			}}
+			
+			return production.values.toSet()
+			
+		}
+	
 }
