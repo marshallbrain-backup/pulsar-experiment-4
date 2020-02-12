bfdea8c5f7fe86726b69c334d67e5bc01227dcc1
marshallbrain <marshalldbrain@gmail.com>
2020-02-12 13:29:41 -0500

    Rework ConstructionTask

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/construction/ConstructionTask.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/construction/ConstructionTask.kt
@@ -4,23 +4,25 @@ import com.marshalldbrain.pulsar.colony.districts.District
 import com.marshalldbrain.pulsar.resources.Resource
 import kotlin.math.absoluteValue
 
-class ConstructionTask(
+class ConstructionTask (
+	val id: String,
 	val type: Type,
-	val target: Constructable,
-	val amount: Int
+	val time: Int,
+	val amount: Int,
+	val totalCost: Set<Resource>,
+	val onComplete: () -> Unit
 ) {
 	
 	//TODO move creation to the Constructable
 	//TODO add onComplete value
 	
-	
-	var timeRemaining: Int = amount * target.time
+	var timeRemaining: Int = amount * time
 		private set
 	val amountRemaining: Int
-		get() = timeRemaining / target.time + 1
+		get() = timeRemaining / time + 1
 	
 	val projectName: String
-		get() = type.name(target)
+		get() = "$type $id"
 	
 	fun passTime(timePass: Int): Int {
 		val remaining = timeRemaining - timePass
@@ -35,12 +37,10 @@ class ConstructionTask(
 	enum class Type {
 		
 		BUILD {
-			override fun name(target: Constructable): String {
-				return "Building ${target.name}"
+			override fun toString(): String {
+				return "Building"
 			}
-		};
-		
-		abstract fun name(target: Constructable): String
+		}
 		
 	}
 	
