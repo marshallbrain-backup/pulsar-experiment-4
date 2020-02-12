4cbf57efd932e9eecdc293c6f8f98dacd8d2625c
marshallbrain <marshalldbrain@gmail.com>
2020-02-12 13:36:04 -0500

    Implement the ResourceCollection interface

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceBucket.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceBucket.kt
@@ -1,18 +1,17 @@
 package com.marshalldbrain.pulsar.resources
 
-class ResourceBucket (vararg sources: List<ResourceCollection>) {
+class ResourceBucket (vararg sources: List<ResourceCollection>) : ResourceCollection {
 	
 	private val sourceList = sources.asList()
 	private val bucket = mutableMapOf<ResourceType, Resource>()
 	
-	val totalProduction: Set<Resource>
+	override val income: Set<Resource>
 		get() {
 			
 			val production = mutableMapOf<ResourceType, Resource>()
 			
 			sourceList.forEach { s -> s.forEach {
-				production += it.production
-				production -= it.upkeep
+				production += it.income
 			}}
 			
 			return production.values.toSet()
