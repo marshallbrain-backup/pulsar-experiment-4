d1a95a69cd4f86feaae009a299449a231a9246bb
marshallbrain <marshalldbrain@gmail.com>
2020-02-23 23:21:48 -0500

    Rework district creation

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/Colony.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/Colony.kt
@@ -1,39 +1,17 @@
 package com.marshalldbrain.pulsar.colony
 
 import com.marshalldbrain.pulsar.colony.construction.ConstructionManager
-import com.marshalldbrain.pulsar.colony.districts.District
-import com.marshalldbrain.pulsar.colony.districts.DistrictType
+import com.marshalldbrain.pulsar.colony.production.DistrictManager
+import com.marshalldbrain.pulsar.colony.production.DistrictType
 
-data class Colony(private val allDistrictTypes: Set<DistrictType>) {
+class Colony(allDistrictTypes: Set<DistrictType>) {
 	
-	val districts = MutableList(5) {
-		District()
-	}
 	val constructionManager = ConstructionManager()
 	
-	init {
-		
-		val startingDistrictTypes = districtTypes.filter { it.starting }.sortedBy { it.id }
-		for (i in min(districts.indices, startingDistrictTypes.indices)) {
-			districts[i].type = startingDistrictTypes[i]
-		}
-		
-	}
+	val districts = DistrictManager(allDistrictTypes)
 	
 	private fun min(a: IntRange, b: IntRange): IntRange {
 		return if (a.last > b.last) b else a
 	}
-	
-	val untooledDistricts: List<District>
-		get() {
-			return districts.filter { it.type == DistrictType.emptyDistrict }
-		}
-	
-	val districtTypes: Set<DistrictType>
-		get() {
-			return allDistrictTypes.filter { type ->
-				type.possible and !districts.any { it.type == type }
-			}.toSet()
-		}
 	
 }
