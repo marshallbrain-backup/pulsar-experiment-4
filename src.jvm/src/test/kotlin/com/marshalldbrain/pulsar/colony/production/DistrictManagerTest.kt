a2f0232890b69fe696fa2b43600d40eb84080db0
marshallbrain <marshalldbrain@gmail.com>
2020-02-27 08:15:13 -0500

    Add tests for task creation

--- src.jvm/src/test/kotlin/com/marshalldbrain/pulsar/colony/production/DistrictManagerTest.kt
+++ src.jvm/src/test/kotlin/com/marshalldbrain/pulsar/colony/production/DistrictManagerTest.kt
@@ -1,7 +1,11 @@
 package com.marshalldbrain.pulsar.colony.production
 
+import com.marshalldbrain.pulsar.colony.construction.ConstructionManager
+import com.marshalldbrain.pulsar.colony.construction.ConstructionType
+import io.kotlintest.matchers.collections.shouldBeEmpty
 import io.kotlintest.matchers.collections.shouldContainExactly
 import io.kotlintest.matchers.maps.shouldContainKeys
+import io.kotlintest.matchers.maps.shouldNotContainExactly
 import io.kotlintest.specs.FunSpec
 import io.kotlintest.shouldBe
 
@@ -43,7 +47,41 @@ class DistrictManagerTest : FunSpec({
 		
 		val dm = DistrictManager(districts)
 		
-		dm.districts.shouldContainKeys(*expected.toTypedArray())
+		dm.districts.keys.shouldContainExactly(expected)
 		
 	}
+	
+	context("Create task") {
+		
+		test("Multiple amount") {
+			
+			val type = DistrictType("1", 5, starting = true)
+			
+			val dm = DistrictManager(setOf(type))
+			val cm = ConstructionManager()
+			
+			val task = dm.createConstructionTask(type, ConstructionType.BUILD, 4)
+			cm.addToQueue(task)
+			cm.tick(20)
+			
+			dm.districts[type] shouldBe 4
+			cm.constructionQueue.shouldBeEmpty()
+			
+		}
+		
+		test("Build task") {
+			
+			val type = DistrictType("1", starting = true)
+			
+			val dm = DistrictManager(setOf(type))
+			
+			val task = dm.createConstructionTask(type, ConstructionType.BUILD, 1)
+			task.runOnComplete()
+			
+			dm.districts[type] shouldBe 1
+			
+		}
+		
+	}
+	
 })
