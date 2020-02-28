5cdb3962baf5bb20681eb58ca06cdefdc95a464e
marshallbrain <marshalldbrain@gmail.com>
2020-02-28 17:29:52 -0500

    Implement ResourcePing and add change modifier

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceIncome.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceIncome.kt
@@ -1,21 +1,21 @@
 package com.marshalldbrain.pulsar.resources
 
-class ResourceIncome {
+class ResourceIncome : ResourcePing {
 	
 	private val mutableIncome = mutableMapOf<ResourceType, Int>()
 	
 	val income: Map<ResourceType, Int>
 		get() = mutableIncome
 	
-	fun pingChange(change: Map<ResourceType, Int>) {
+	override fun pingChange(change: Map<ResourceType, Int>, modifier: Int) {
 		
 		for (k in change.keys) {
 			val a = mutableIncome[k]?: 0
 			val b = change[k]?: 0
 			if (a != 0 && b != 0) {
-				mutableIncome[k] = a + b
+				mutableIncome[k] = a + b * modifier
 			} else {
-				mutableIncome[k] = b
+				mutableIncome[k] = b * modifier
 			}
 		}
 		
