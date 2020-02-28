7acc3e2180639f3a31fbc8082bafe728d2d992bc
marshallbrain <marshalldbrain@gmail.com>
2020-02-28 10:49:08 -0500

    Add ability to update income

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceIncome.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceIncome.kt
@@ -1,4 +1,24 @@
 package com.marshalldbrain.pulsar.resources
 
 class ResourceIncome {
+	
+	private val mutableIncome = mutableMapOf<ResourceType, Int>()
+	
+	val income: Map<ResourceType, Int>
+		get() = mutableIncome
+	
+	fun pingChange(change: Map<ResourceType, Int>) {
+		
+		for (k in change.keys) {
+			val a = mutableIncome[k]?: 0
+			val b = change[k]?: 0
+			if (a != 0 && b != 0) {
+				mutableIncome[k] = a + b
+			} else {
+				mutableIncome[k] = b
+			}
+		}
+		
+	}
+	
 }
