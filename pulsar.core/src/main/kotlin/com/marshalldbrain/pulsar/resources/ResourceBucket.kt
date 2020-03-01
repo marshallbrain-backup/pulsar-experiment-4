81c3b6d7e8a5ae8d7a9934d4590fd7b06e898f01
marshallbrain <marshalldbrain@gmail.com>
2020-02-29 22:58:27 -0500

    Add processIncome function
    Gets the current income and adds it to the total.

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceBucket.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceBucket.kt
@@ -1,13 +1,17 @@
 package com.marshalldbrain.pulsar.resources
 
+import com.marshalldbrain.ion.collections.getOrDefault
+
 class ResourceBucket() : ResourceTeller {
 
 	private val total = mutableMapOf<ResourceType, Int>()
 	private val income = ResourceIncome()
 	
+	override val incomeUpdater: ResourceUpdater
+		get() = income
 	val bucket: Map<ResourceType, Pair<Int, Int>>
-		get() = total.mapValues {
-			Pair(it.value, income.income[it.key]?: 0)
+		get() = total.mapValues { (k, v) ->
+			Pair(v, income.income[k]?: 0)
 		}
 	
 	override fun deposit(amount: Pair<ResourceType, Int>) {
@@ -29,5 +33,11 @@ class ResourceBucket() : ResourceTeller {
 			total[it.key] = (total[it.key]?: 0) - it.value
 		}
 	}
-
+	
+	fun processIncome() {
+		income.income.forEach { (k, v) ->
+			total[k] = total.getOrDefault(k, 0) + v
+		}
+	}
+	
 }
--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceTeller.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/resources/ResourceTeller.kt
@@ -2,6 +2,8 @@ package com.marshalldbrain.pulsar.resources
 
 interface ResourceTeller {
 	
+	val incomeUpdater: ResourceUpdater
+	
 	fun deposit(amount: Pair<ResourceType, Int>)
 	fun depositAll(amount: Map<ResourceType, Int>)
 	fun withdraw(amount: Pair<ResourceType, Int>)
