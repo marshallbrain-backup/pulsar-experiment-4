b5dd5838219d6d5606a0d8d4898fdfcf7017dd98
Marshall David Brain <marshalldbrain@gmail.com>
2020-03-02 23:04:20 -0500

    Fix crashes and replace pair with list

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/production/DistrictManager.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/production/DistrictManager.kt
@@ -1,18 +1,19 @@
 package com.marshalldbrain.pulsar.colony.production
 
+import com.marshalldbrain.ion.collections.getOrDefault
 import com.marshalldbrain.pulsar.colony.construction.ConstructionTask
 import com.marshalldbrain.pulsar.colony.construction.ConstructionType
 import com.marshalldbrain.pulsar.resources.ResourceUpdater
 
 class DistrictManager(
 	private val allDistrictTypes: Set<DistrictType>,
-	private val resourceUpdater: ResourceUpdater,
-	private val jobUpdater: JobUpdater
+	private val resource: ResourceUpdater,
+	private val job: JobUpdater
 ) : DistrictInfo {
 	
 	override val max = 5
 	
-	private val mutableDistrictMap = possibleTypes
+	private val map = possibleTypes
 		.filter { it.starting }
 		.take(max)
 		.associateWith { 0 }
@@ -25,51 +26,47 @@ class DistrictManager(
 	override val remainingTypes: Set<DistrictType>
 		get() = allDistrictTypes - districts.keys
 	override val districts: Map<DistrictType, Int>
-		get() = mutableDistrictMap
+		get() = map
 	
 	override fun createConstructionTask(
-		target: Pair<DistrictType, DistrictType>,
+		target: List<DistrictType>,
 		type: ConstructionType,
 		amount: Int
 	): ConstructionTask {
-		
-		if (!districts.containsKey(target.first)) {
-			throw NoSuchElementException()
-		}
 		
 		return when (type) {
 			
 			ConstructionType.BUILD -> {
 				ConstructionTask(
-					"Building ${target.first}",
-					target.first.cost, target.first.time, amount
+					"Building ${target[0].id}",
+					target[0].cost, target[0].time, amount
 				) { OnComplete().build(target) }
 			}
 			
 			ConstructionType.DESTROY -> {
 				ConstructionTask(
-					"Destroying ${target.first}",
+					"Destroying ${target[0].id}",
 					emptyMap(), 0, amount
 				) { OnComplete().destroy(target) }
 			}
 			
 			ConstructionType.REPLACE -> {
 				ConstructionTask(
-					"Replacing ${target.first} with ${target.second}",
-					target.second.cost, target.second.time, amount
+					"Replacing ${target[1].id} with ${target[0].id}",
+					target[0].cost, target[0].time, amount
 				) { OnComplete().replace(target) }
 			}
 			
 			ConstructionType.TOOL -> {
 				ConstructionTask(
-					"Tooling ${target.first} to ${target.second}",
-					target.second.cost, target.second.time, amount
+					"Tooling ${if(target.size > 1) target[1].id else "untooled"} to ${target[0].id}",
+					target[0].cost, target[0].time, amount
 				) { OnComplete().tool(target) }
 			}
 			
 			ConstructionType.DETOOL -> {
 				ConstructionTask(
-					"Detooling ${target.first}",
+					"Detooling ${target[0].id}",
 					emptyMap(), 0, amount
 				) { OnComplete().detool(target) }
 			}
@@ -79,88 +76,48 @@ class DistrictManager(
 		
 	}
 	
+	private fun modifyDistrict(target: DistrictType, amount: Int) {
+		
+		map[target] = map.getValue(target) + amount
+		
+		resource.update(target.production, amount)
+		resource.update(target.upkeep, -amount)
+		job.update(target.jobs, amount)
+		
+	}
+	
 	private inner class OnComplete {
 		
-		fun build(target: Pair<DistrictType, DistrictType>) {
-			
-			mutableDistrictMap[target.first] = mutableDistrictMap.getValue(target.first) + 1
-			
-			resourceUpdater.update(target.first.production)
-			resourceUpdater.update(target.first.upkeep, -1)
-			jobUpdater.update(target.first.jobs)
-			
+		fun build(target: List<DistrictType>) {
+			modifyDistrict(target[0], 1)
 		}
 		
-		fun destroy(target: Pair<DistrictType, DistrictType>) {
-			
-			if (mutableDistrictMap[target.first]!! > 0) {
-				
-				mutableDistrictMap[target.first] = mutableDistrictMap.getValue(target.first) - 1
-				
-				resourceUpdater.update(target.first.production, -1)
-				resourceUpdater.update(target.first.upkeep)
-				jobUpdater.update(target.first.jobs, -1)
-				
+		fun destroy(target: List<DistrictType>) {
+			if (map[target[0]]!! > 0) {
+				modifyDistrict(target[0], -1)
 			}
-			
 		}
 		
-		fun replace(target: Pair<DistrictType, DistrictType>) {
-			
-			if (mutableDistrictMap[target.first]!! > 0) {
-				
-				mutableDistrictMap[target.first] = mutableDistrictMap.getValue(target.first) - 1
-				
-				resourceUpdater.update(target.first.production, -1)
-				resourceUpdater.update(target.first.upkeep)
-				jobUpdater.update(target.first.jobs, -1)
-				
-				mutableDistrictMap[target.second] = mutableDistrictMap.getValue(target.second) + 1
-				
-				resourceUpdater.update(target.second.production)
-				resourceUpdater.update(target.second.upkeep, -1)
-				jobUpdater.update(target.second.jobs)
-				
+		fun replace(target: List<DistrictType>) {
+			if (map[target[1]]!! > 0) {
+				modifyDistrict(target[1], -1)
+				modifyDistrict(target[0], 1)
 			}
-			
 		}
 		
-		fun tool(target: Pair<DistrictType, DistrictType>) {
-			
-			if (mutableDistrictMap.containsKey(target.first)) {
-				
-				val amount = mutableDistrictMap[target.first] ?: 0
-				
-				resourceUpdater.update(target.second.production, amount)
-				resourceUpdater.update(target.second.upkeep, -amount)
-				jobUpdater.update(target.second.jobs, amount)
-				
-				mutableDistrictMap[target.second] = amount
-				
-				resourceUpdater.update(target.first.production, -amount)
-				resourceUpdater.update(target.first.upkeep, amount)
-				jobUpdater.update(target.first.jobs, -amount)
-				
-				mutableDistrictMap.remove(target.first)
-				
+		fun tool(target: List<DistrictType>) {
+			if (target.size > 1 && map.containsKey(target[1])) {
+				modifyDistrict(target[1], map.getValue(target[1]))
+				map.remove(target[1])
 			}
-			
+			map[target[0]] = 0
 		}
 		
-		fun detool(target: Pair<DistrictType, DistrictType>) {
-			
-			if (mutableDistrictMap.containsKey(target.first)) {
-				
-				val amount = mutableDistrictMap[target.first] ?: 0
-				
-				resourceUpdater.update(target.first.production, -amount)
-				resourceUpdater.update(target.first.upkeep, amount)
-				jobUpdater.update(target.first.jobs, -amount)
-				
-				mutableDistrictMap.remove(target.first)
-				
+		fun detool(target: List<DistrictType>) {
+			if (map.containsKey(target[0])) {
+				modifyDistrict(target[0], map.getValue(target[0]))
+				map.remove(target[0])
 			}
-			
 		}
 	
 	}
@@ -176,7 +133,7 @@ interface DistrictInfo {
 	val remainingTypes: Set<DistrictType>
 	
 	fun createConstructionTask(
-		target: Pair<DistrictType, DistrictType>,
+		target: List<DistrictType>,
 		type: ConstructionType, amount: Int
 	): ConstructionTask
 	
