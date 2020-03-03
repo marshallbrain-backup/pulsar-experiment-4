af7b25615f95bbb3a840933b813b9d0978ce6ff8
Marshall David Brain <marshalldbrain@gmail.com>
2020-03-02 21:43:51 -0500

    Remove unneeded line in the update function

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/production/JobManager.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/production/JobManager.kt
@@ -12,8 +12,6 @@ class JobManager(private val resourceUpdater: ResourceUpdater) : JobUpdater {
 	
 	override fun update(change: Map<JobType, Int>, modifier: Int) {
 		
-		val resources = mutableMapOf<ResourceType, Int>()
-		
 		change.forEach { (k, v) ->
 			mutableJobs[k] = mutableJobs[k]?:0 + v * 1
 			resourceUpdater.update(k.production, v)
