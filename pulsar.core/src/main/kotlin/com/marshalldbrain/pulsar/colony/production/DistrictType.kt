ad60a445ccbd28eb9b9ff608b1396baf73c12219
Marshall David Brain <marshalldbrain@gmail.com>
2020-03-02 21:43:14 -0500

    Remove the Constructible interface
    Cost and time information is now given to ConstructionTask directly.

--- pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/production/DistrictType.kt
+++ pulsar.core/src/main/kotlin/com/marshalldbrain/pulsar/colony/production/DistrictType.kt
@@ -1,24 +1,14 @@
 package com.marshalldbrain.pulsar.colony.production
 
-import com.marshalldbrain.pulsar.colony.construction.Constructible
 import com.marshalldbrain.pulsar.resources.ResourceType
 
 data class DistrictType (
-	override val id: String,
-	override val time: Int = 0,
+	val id: String,
+	val time: Int = 0,
 	val possible: Boolean = true,
 	val starting: Boolean = false,
-	override val cost: Map<ResourceType, Int> = emptyMap(),
+	val cost: Map<ResourceType, Int> = emptyMap(),
 	val production: Map<ResourceType, Int> = emptyMap(),
 	val upkeep: Map<ResourceType, Int> = emptyMap(),
 	val jobs: Map<JobType, Int> = emptyMap()
-) : Constructible {
-	
-	val isTooled: Boolean
-		get() = this != emptyDistrict
-	
-	companion object {
-		val emptyDistrict = DistrictType("Untooled")
-	}
-	
-}
+)
